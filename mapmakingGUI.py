c209a1f88af3e0f4fa54846579a9602020acfa39
Hyunse <99864704+kim-hyunse@users.noreply.github.com>
2023-11-29 02:20:42 +0900

    Add files via upload
    add roscore

--- mapmakingGUI.py
+++ mapmakingGUI.py
@@ -11,6 +11,16 @@ class ROSBagToMapGUI:
         self.root = root
         root.title("ROS Bag to Map")
 
+        # Start roscore
+        roscore_cmd = "roscore"
+        subprocess.Popen(roscore_cmd, shell=True)
+
+        # Initialize ROS node
+        rospy.init_node('rosbag_to_map_gui', anonymous=True)
+
+        # Create a ROS publisher to send the stop signal
+        self.stop_lego_loam_pub = rospy.Publisher('stop_lego_loam', Empty, queue_size=1)
+
         # Create a label to instruct the user
         label = tk.Label(root, text="Select a ROS bag file and click 'Make a Map'")
         label.pack(padx=10, pady=10)
@@ -20,7 +30,7 @@ class ROSBagToMapGUI:
         self.select_bag_button.pack(padx=10, pady=5)
 
         # Create a button to make a map
-        self.make_map_button = tk.Button(root, text="Make a Map", command=self.launch_rosbag)
+        self.make_map_button = tk.Button(root, text="Make a Map", command=self.launch_lego_loam)
         self.make_map_button.pack(padx=10, pady=10)
         self.make_map_button.config(state=tk.DISABLED)  # Disable the button initially
 
@@ -33,50 +43,25 @@ class ROSBagToMapGUI:
         self.rosbag_process = None
         self.lego_loam_process = None
 
-        # Initialize ROS node
-        rospy.init_node('rosbag_to_map_gui', anonymous=True)
-
-        # Create a ROS publisher to send the stop signal
-        self.stop_lego_loam_pub = rospy.Publisher('stop_lego_loam', Empty, queue_size=1)
-
     def select_rosbag(self):
         self.selected_rosbag = filedialog.askopenfilename(filetypes=[("ROS Bags", "*.bag")])
         if self.selected_rosbag:
             self.make_map_button.config(state=tk.NORMAL)  # Enable the "Make a Map" button
 
-    def launch_rosbag(self):
+    def launch_lego_loam(self):
         if self.selected_rosbag:
             # Replace 'your_package' and 'your_launch_file' with the actual package and launch file names for lego_loam
-
             lego_loam_cmd = f"roslaunch lego_loam run.launch"
             rosbag_play_cmd = f"rosbag play {self.selected_rosbag} --clock --topic /ouster/points -r 3"
 
             # Start lego_loam process using roslaunch
             self.lego_loam_process = subprocess.Popen(lego_loam_cmd, shell=True)
 
-            # Wait for lego_loam to start (adjust the delay time as needed)
-            self.root.after(3000, self.start_rosbag_play, rosbag_play_cmd)
-
-    def start_rosbag_play(self, rosbag_play_cmd):
-        # Start rosbag play process
-        self.rosbag_process = subprocess.Popen(rosbag_play_cmd, shell=True)
-
-        # Enable the "Stop Lego Loam" button while rosbag is running
-        self.stop_lego_loam_button.config(state=tk.NORMAL)
-
-        # Disable the "Make a Map" button while rosbag is running
-        self.make_map_button.config(state=tk.DISABLED)
-
-        # Start a thread to monitor the rosbag process
-        self.root.after(100, self.monitor_rosbag_process)
-
-    def monitor_rosbag_process(self):
-        if self.rosbag_process.poll() is None:
-            # Rosbag play is still running, check again after 100 ms
-            self.root.after(100, self.monitor_rosbag_process)
-        else:
-            # Rosbag play has completed, enable the "Stop Lego Loam" button
+            # Enable the "Stop Lego Loam" button while lego_loam is running
             self.stop_lego_loam_button.config(state=tk.NORMAL)
+
+            # Disable the "Make a Map" button while lego_loam is running
+            self.make_map_button.config(state=tk.DISABLED)
 
     def stop_lego_loam(self):
         if self.lego_loam_process is not None:
